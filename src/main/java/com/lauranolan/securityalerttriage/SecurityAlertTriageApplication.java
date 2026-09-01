9da76c4e7802d4ea8a49f38b79fbe289a18d997f
Laura Nolan <318711154+laura-e-nolan@users.noreply.github.com>
2026-09-01 17:58:10 -0400

    Initialize Spring Boot project

---
+++ src/main/java/com/lauranolan/securityalerttriage/SecurityAlertTriageApplication.java
@@ -0,0 +1,13 @@
+package com.lauranolan.securityalerttriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAlertTriageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAlertTriageApplication.class, args);
+	}
+
+}
